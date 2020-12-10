1179b486c50d73d264b1fcb32e32723a72d06a0d
张峰 <3230340338@qq.com>
2020-12-11 03:32:04 +0800

    添加文件

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package main.java;
+
+/**
+ *@Description  
+ *@author 张峰 Email:3230340338@qq.com
+ *@version
+ *@date2020年12月11日上午3:21:48
+ */
+public class HelloGit {
+      public void heihei() {
+    	  System.out.print("Hello Git");
+      }
+}
